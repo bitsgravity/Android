377ad04ae14d551b4a813298bfa46fcd78f23fcc
Nirmal CM <nirmal.cm@oneindia.co.in>
2022-07-13 16:14:26 +0530

    Support | Logger and views added

---
+++ support/src/main/java/com/bits/support/logger/Logger.kt
@@ -0,0 +1,12 @@
+package com.bits.support.logger
+
+import android.util.Log
+import com.bits.support.logger.LOG.TAG
+
+object LOG {
+    const val TAG = "ncm"
+}
+
+fun log(message : String){
+    Log.d(TAG,message)
+}
